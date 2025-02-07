8dbd6e618965ca3a4d6e16208f11422d0ec42cf0
amangupta25 <amangupta570@gmail.com>
2025-02-08 03:19:10 +0530

    Revert "correct log"
    This reverts commit 108c99642d6a71cf3c5a5b03deb0e25ef471b7dd.

--- services/engine/dataherald/sql_generator/dataherald_sqlagent.py
+++ services/engine/dataherald/sql_generator/dataherald_sqlagent.py
@@ -368,7 +368,7 @@ class ColumnEntityChecker(BaseSQLDatabaseTool, BaseTool):
             schema_name = None
             print("superman before")
             print(table_name)
-            print(traceback.print_stack())
+            print(traceback.print_exc())
             if "." in table_name:
                 print("superman inside happy path")
                 schema_name, table_name = table_name.split(".")
